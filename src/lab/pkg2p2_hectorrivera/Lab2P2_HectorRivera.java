37e4a24a761561395e059b128edf488c30aa3ff0
unknown <hrolriverav@gmail.com>
2024-01-26 14:11:37 -0600

    commit3

--- src/lab/pkg2p2_hectorrivera/Lab2P2_HectorRivera.java
+++ src/lab/pkg2p2_hectorrivera/Lab2P2_HectorRivera.java
@@ -4,17 +4,43 @@
  */
 package lab.pkg2p2_hectorrivera;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author hrolriverav
  */
 public class Lab2P2_HectorRivera {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    static ArrayList<Usuario> UserList = new ArrayList<Usuario>();
     
+    public static void main(String[] args) {
+        Usuario User = new Usuario();
+        StartUsers();
+        int opc = 0;
+        while(opc == 0){
+            String username = JOptionPane.showInputDialog("Ingrese su User");
+            int found = User.verUser(UserList, username);
+            if (found!=-1) {
+                String contraseña = JOptionPane.showInputDialog("Ingrese su Contraseña");
+                if (true) {
+                    
+                }else{
+                    
+                }
+            }else{
+            JOptionPane.showMessageDialog(null, "No se encontro el usario");
+            }
+
+        }
+    }
+    public static void StartUsers(){
+        Usuario User1 = new Usuario("Hector", "7821A","Estudiante");
+        Usuario User2 = new Usuario("Jimenez","91p0", "profesor");
+        Usuario User3 = new Usuario("Andrea","?12wa", "bibliotecario");
+        UserList.add(User1);
+        UserList.add(User2);
+        UserList.add(User3); 
+    }
 }
---
+++ src/lab/pkg2p2_hectorrivera/Usuario.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg2p2_hectorrivera;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hrolriverav
+ */
+public class Usuario {
+    
+    private String name;
+    private String Contraseña;
+    private String Tipo;
+    
+    public Usuario(){  
+    }
+    
+    public Usuario(String name,String Contraseña,String Tipo) {
+        this.name=name;
+        this.Contraseña=Contraseña;
+        this.Tipo=Tipo;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+    
+    public int verUser(ArrayList<Usuario> Lista, String UserSent){
+        for (int i = 0; i < Lista.size(); i++) {
+            if (Lista.get(i).getName()== UserSent) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public boolean verContra(ArrayList<Usuario> Lista, String ContraSent){
+        for (int i = 0; i < Lista.size(); i++) {
+            if (Lista.get(i).getContraseña()== ContraSent) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
